8f22b04b3786fa5286e453d6024ad2df6ecb8382
Ayobami <ayobamiadepoju263@gmail.com>
2026-01-30 09:03:35 +0100

    initial commit

---
+++ src/main/java/GitOperations.java
@@ -0,0 +1,15 @@
+package main.java;
+
+public class GitOperations {
+
+    public static void main(String[] args) {
+
+        System.out.println("Learning GIT Operations");
+        System.out.println("git init");
+        System.out.println("git add .");
+        System.out.println("git status");
+        System.out.println("git commit -m \"commit message\"");
+
+        System.out.println();
+    }
+}
